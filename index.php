0898fd61868c4ab19981c28681841d04cf70c706
Donny van Walsem <donnehvw@gmail.com>
2025-03-13 12:06:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php
-//include "vendor/autoload.php";
-//$data = [
-//    'username' => 'fff',
-//];
-//
-//$rules = [
-//    'username' => 'max:2',
-//];
-//$validator = new \Donneh\Validator($data, $rules);
-//var_dump($validator->errors());
+include "vendor/autoload.php";
+$data = [
+   'username' => 'fff',
+];
 
-function isOfDrivingAge($age) {
-    return $age >= 18;
-}
-
-function notifyOfDrivingAge($name, $age) {
-    return $message = isOfDrivingAge($age) ? 'May drive.' : 'May not drive.';
-}
-
-echo notifyOfDrivingAge('Yannick', 21);
+$rules = [
+   'username' => 'max:2',
+];
+$validator = new \Donneh\Validator($data, $rules);
+var_dump($validator->errors());
